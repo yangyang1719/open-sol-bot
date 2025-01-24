3d2ca3bb33ef8393aed2b01092620ae8d7070877
mkdir700 <mkdir700@gmail.com>
2025-01-24 21:09:44 +0800

    Update swap notification keyboard with new buttons and links
    - Add a "立即交易" button with callback functionality to refresh swap transactions.
    - Include additional bot links for GMGN, Degoee, SolTrandingBot, and Pepeboost.
    - Remove the "查看交易" button and its associated URL to streamline the interface.

--- src/tg_bot/keyboards/notify_swap.py
+++ src/tg_bot/keyboards/notify_swap.py
@@ -1,28 +1,36 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
 from common.types.tx import TxEvent
 
 
 def notify_swap_keyboard(tx_event: TxEvent) -> InlineKeyboardMarkup:
     return InlineKeyboardMarkup(
         inline_keyboard=[
-            # [
-            #     InlineKeyboardButton(
-            #         text="立即交易",
-            #         callback_data="swap",
-            #     )
-            # ],
             [
                 InlineKeyboardButton(
-                    text="查看交易",
-                    url=f"https://solscan.io/tx/{tx_event.signature}",
+                    text="立即交易",
+                    callback_data=f"swap:refresh_{tx_event.mint}",
                 )
             ],
-            # 跳转到其他 bot 去交易
             [
                 InlineKeyboardButton(
                     text="GMGN",
                     url=f"https://t.me/GMGN_sol_bot?start={tx_event.mint}",
-                )
+                ),
+                InlineKeyboardButton(
+                    text="Degoee",
+                    url=f"https://t.me/dogeebot_bot?start={tx_event.mint}",
+                ),
+            ],
+            [
+                InlineKeyboardButton(
+                    text="SolTrandingBot",
+                    url=f"https://t.me/SolTradingBot?start={tx_event.mint}",
+                ),
+                InlineKeyboardButton(
+                    text="Pepeboost",
+                    url=f"https://t.me/pepeboost_sol05_bot?start={tx_event.mint}",
+                ),
             ],
         ]
     )
