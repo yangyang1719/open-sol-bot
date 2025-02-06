06d319ca62d590dd96db137ef057f9c2c83704d9
mkdir700 <mkdir700@gmail.com>
2025-02-06 16:28:29 +0800

    feat: 触发跟单和跟单结果将通知到用户

--- src/tg_bot/notify/notify.py
+++ src/tg_bot/notify/notify.py
@@ -1,19 +1,28 @@
+import asyncio
+
 import aioredis
 from aiogram import Bot
 
 from .smart_swap import SmartWalletSwapAlertNotify
 from .swap_result import SwapResultNotify
+from .copytrade_trigger import CopyTradeNotify
 
 
 class Notify:
     def __init__(self, redis: aioredis.Redis, bot: Bot):
         self.smart_wallet_swap_notify = SmartWalletSwapAlertNotify(redis, bot)
         self.swap_result_notify = SwapResultNotify(redis, bot)
+        self.copytrade_notify = CopyTradeNotify(redis, bot)
 
     async def start(self):
-        await self.smart_wallet_swap_notify.start()
-        await self.swap_result_notify.start()
+        tasks = [
+            self.smart_wallet_swap_notify.start(),
+            self.swap_result_notify.start(),
+            self.copytrade_notify.start(),
+        ]
+        await asyncio.gather(*tasks)
 
     async def stop(self):
         self.smart_wallet_swap_notify.stop()
         self.swap_result_notify.stop()
+        self.copytrade_notify.stop()
